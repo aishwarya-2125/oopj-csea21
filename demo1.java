4ea5093bd96c154bd2dbf29ecc827bee1e8aa813
aishwarya-2125 <aishwaryah2506@gmail.com>
2026-03-04 12:32:17 +0530

    week5

---
+++ demo1.java
@@ -0,0 +1,13 @@
+// package p1;
+import p1.protection;
+import p1.derived;
+import p1.samepackage;
+public class demo1
+{
+	public static void main(String args[])
+	{
+		protection ob1= new protection();
+		derived ob2= new derived();
+		samepackage ob3= new samepackage();
+	}
+}
